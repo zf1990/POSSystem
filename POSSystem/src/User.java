2d0b68f6cf6cb17390d52d4dcc29a34bb630a693
fadi bchara <fadibshara-1987@hotmail.com>
2019-12-08 14:37:45 -0600

    Attempt2

---
+++ POSSystem/src/User.java
@@ -0,0 +1,71 @@
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class User {
+
+	private String UserName;
+	private String Password;
+	private String FirstName;
+	private String LastName;
+	private String EmailAddress;
+	private String PhoneNumber;
+	private String ChangeUserName;
+	private String ChangePassword;
+	private int UserId;
+
+	public User(String userName, String password, String firstName, String lastName, String emailAddress,
+			String phoneNumber) {
+
+		this.UserName = userName;
+		this.Password = password;
+		this.FirstName = firstName;
+		this.LastName = lastName;
+		this.EmailAddress = emailAddress;
+		this.PhoneNumber = phoneNumber;
+	}
+
+	public void UserDictionary() {
+		Dictionary<String, Integer> UserDictionary = new Hashtable<String, Integer>();
+
+		UserDictionary.put(UserName, UserId);
+		UserDictionary.put(Password, UserId);
+		UserDictionary.put(FirstName, UserId);
+		UserDictionary.put(LastName, UserId);
+		UserDictionary.put(PhoneNumber, UserId);
+		UserDictionary.put(EmailAddress, UserId);
+
+	}
+
+	public int UserId() {
+		return this.UserId;
+	}
+
+	public String GetUserName() {
+		return this.UserName;
+	}
+
+	public String GetPassword() {
+		return this.Password;
+	}
+
+	public String GetFirstName() {
+		return this.FirstName;
+	}
+
+	public String GetLastName() {
+		return this.LastName;
+	}
+
+	public String ChangeUserName(String UserName) {
+		return this.ChangeUserName;
+	}
+
+	public String ChangePassword(String Password) {
+		return this.ChangePassword;
+	}
+
+	public String toString() {
+		return FirstName + "" + LastName + "" + PhoneNumber + "" + EmailAddress;
+	}
+}
