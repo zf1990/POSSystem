699e3db8a6490cf1aea22d8e6139f5d8e2e2cccd
zf1990 <fang1534@stthomas.edu>
2019-12-08 20:55:56 -0600

    Zhou's commit

--- POSSystem/src/Inventory.java
+++ POSSystem/src/Inventory.java
@@ -4,40 +4,63 @@
  * 12/13/2019
  */
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Inventory {
 	private String name;
 	private static ArrayList<Item> inventoryList;
+	private static int itemCount;
+	private String path;
 	
 	public Inventory(String name) {
 		this.name = name;
 		inventoryList = new ArrayList<Item>();
+		itemCount=0;
+		path = "inventoryReport.csv";
 	}
 	
 	public static void addToInventory(Item item) {
 		inventoryList.add(item);
+		itemCount++;
 	}
 	
 	public static void removeItem(Item item) {
 		inventoryList.remove(item);
+		itemCount--;
 	}
 	
 	public void orderItems(Item item) {
-		Item tobeChanged = inventoryList.get(findIndex(item));
-		tobeChanged.setOrderingQuantity(tobeChanged.getOrderQuantity());
+		item.setOrderingQuantity(item.getOrderQuantity());
 	}
 	
 	public void orderReceived(Item item) {
-		Item tobeChanged = inventoryList.get(findIndex(item));
-		tobeChanged.addQuantity(tobeChanged.getOrderingQuantity());
-		tobeChanged.setOrderingQuantity(0);
+		item.addQuantity(item.getOrderingQuantity());
+		item.setOrderingQuantity(0);
 	}
 	
-	public int findIndex(Item item) {
-		int index = inventoryList.indexOf(item);
+//	private int findIndex(Item item) {
+//		int index = inventoryList.indexOf(item);
+//		return index;
+//	}
+	
+	public void printReports() {
+		try {
+			FileWriter csvWriter = new FileWriter(path, false);
+			BufferedWriter buffWriter = new BufferedWriter(csvWriter);
+			PrintWriter pw = new PrintWriter(buffWriter);
+			
+			pw.println("Item Name" + "," + "Current Item Quantity" + "," + "Threshhold" + "," + "Supplier" + "," + "Pending Item Quantity");
+			
+			
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Exception occured.  File not saved.");
+		}
 		
-		return index;
 	}
+	
 
 }
